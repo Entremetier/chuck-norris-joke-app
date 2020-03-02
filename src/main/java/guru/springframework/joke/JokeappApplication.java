98dfa2f9524180655f1c1bd2064b59830294512a
Marco Happe <marco.happe@icloud.com>
2020-03-02 13:20:31 +0100

    added chuck-config.xml

--- src/main/java/guru/springframework/joke/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -2,8 +2,12 @@ package guru.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
+//Beim starten der Application wird der Context über chuck-config.xml geleitet um diese Resource zu laden. Sollte das nicht geschehen
+//wird die Bean "ChuckNorrisQuotes" nicht gefunden und der Context wird nicht geladen.
 public class JokeappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
@@ -4,11 +4,16 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
+//    @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
+
+    //Spring Context sieht diese Klasse nicht mehr beim Starten, die Configuration wird über das chuck-config.xml in den resources gemacht
+    //aus diesem Grund gibt es in JokeappApplication die annotation @ImportResource
+
+    //wenn die Configuration wieder über diese Klasse laufen soll müssen die beiden Annotationen wieder aktiviert werden
 }
